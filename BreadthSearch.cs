7a30b89e0b23c8def75803d5f9512361d3e3e1a6
n-shvedov27 <n.shvedov27@gmail.com>
2018-09-12 10:06:47 +0500

    BreadthSearch

---
+++ BreadthSearch.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadthSearch
+{
+    public class Edge
+    {
+        public readonly Node From;
+        public readonly Node To;
+        public Edge(Node first, Node second)
+        {
+            this.From = first;
+            this.To = second;
+        }
+        public bool IsIncident(Node node)
+        {
+            return From == node || To == node;
+        }
+        public Node OtherNode(Node node)
+        {
+            if (!IsIncident(node)) throw new ArgumentException();
+            if (From == node) return To;
+            return From;
+        }
+    }
+
+    public class Node
+    {
+        readonly List<Edge> edges = new List<Edge>();
+        public readonly int NodeNumber;
+
+        public Node(int number)
+        {
+            NodeNumber = number;
+        }
+
+        public IEnumerable<Node> IncidentNodes
+        {
+            get
+            {
+                return edges.Select(z => z.OtherNode(this));
+            }
+        }
+        public IEnumerable<Edge> IncidentEdges
+        {
+            get
+            {
+                foreach (var e in edges) yield return e;
+            }
+        }
+        public static Edge Connect(Node node1, Node node2, Graph graph)
+        {
+            if (!graph.Nodes.Contains(node1) || !graph.Nodes.Contains(node2)) throw new ArgumentException();
+            var edge = new Edge(node1, node2);
+            node1.edges.Add(edge);
+            node2.edges.Add(edge);
+            return edge;
+        }
+        public static void Disconnect(Edge edge)
+        {
+            edge.From.edges.Remove(edge);
+            edge.To.edges.Remove(edge);
+        }
+    }
+
+    public class Graph
+    {
+        private Node[] nodes;
+
+        public Graph(int nodesCount)
+        {
+            nodes = Enumerable.Range(0, nodesCount).Select(z => new Node(z)).ToArray();
+        }
+
+        public int Length { get { return nodes.Length; } }
+
+        public Node this[int index] { get { return nodes[index]; } }
+
+        public IEnumerable<Node> Nodes
+        {
+            get
+            {
+                foreach (var node in nodes) yield return node;
+            }
+        }
+
+        public void Connect(int index1, int index2)
+        {
+            Node.Connect(nodes[index1], nodes[index2], this);
+        }
+
+        public void Delete(Edge edge)
+        {
+            Node.Disconnect(edge);
+        }
+
+        public IEnumerable<Edge> Edges
+        {
+            get
+            {
+                return nodes.SelectMany(z => z.IncidentEdges).Distinct();
+            }
+        }
+
+        public static Graph MakeGraph(params int[] incidentNodes)
+        {
+            var graph = new Graph(incidentNodes.Max() + 1);
+            for (int i = 0; i < incidentNodes.Length - 1; i += 2)
+                graph.Connect(incidentNodes[i], incidentNodes[i + 1]);
+            return graph;
+        }
+    }
+
+    public static class NodeExtensions
+    {
+        public static IEnumerable<Node> BreadthSearch(this Node startNode)
+        {
+            var visited = new HashSet<Node>();
+            var queue = new Queue<Node>();
+            queue.Enqueue(startNode);
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+                if (visited.Contains(node)) continue;
+                visited.Add(node);
+                yield return node;
+                foreach (var incidentNode in node.IncidentNodes)
+                    queue.Enqueue(incidentNode);
+            }
+        }
+    }
+
+    class BreadthSearch
+    {
+        public static void Main()
+        {
+            var graph = Graph.MakeGraph(
+                0, 1,
+                0, 2,
+                1, 3,
+                1, 4,
+                2, 3,
+                3, 4);
+
+            Console.WriteLine(graph[0]
+                .BreadthSearch()
+                .Select(z => z.NodeNumber.ToString())
+                .Aggregate((a, b) => a + " " + b));
+
+        }
+    }
+}
